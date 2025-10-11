ede9ca6334314ff89b6499c54667211893e4c6a3
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-10-11 16:00:59 -0400

    Scientific-Papers Added: - sn-jnl.cls Modified: - constants.py - scientific.py - scientific_papers.ipynb

--- uml_project/data/constants.py
+++ uml_project/data/constants.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
-
+import typing as t
 
 # PATHS
-ROOT = Path(os.environ.get("DATA_DIR") or Path(__file__).parent.parent).resolve()
-MUSIC_DIR = ROOT / "music/"
-
+ROOT_DIR = Path(os.environ.get("DATA_DIR") or Path(__file__).parent.parent).resolve()
+MUSIC_DIR = ROOT_DIR / "music/"
+SCRATCH_DIR = ROOT_DIR / "scratch/"  # for temporary files
 
 # KAGGLE
 TAYLOR_SWIFT_KAGGLE = "ishikajohari/taylor-swift-all-lyrics-30-albums"
@@ -13,3 +13,10 @@ TAYLOR_SWIFT_KAGGLE = "ishikajohari/taylor-swift-all-lyrics-30-albums"
 
 # GENIUS
 GENIUS_ACCESS_TOKEN = os.environ.get("GENIUS_ACCESS_TOKEN")  # Store in .env file
+
+
+# Dtypes
+class TexSource(t.TypedDict):
+    id: t.Optional[str]
+    files: list[dict[str, str]]  # {"name": str, "text": str}
+    main: t.Optional[str]  # main .tex file name

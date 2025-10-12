e90ee37ac14fd3988f342d220671d406611ff9e6
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-10-11 20:34:15 -0400

    Batch-Scientific-Data Added: - registry.json - t_sne_exaggerates_clusters_provably__1_false_positives.json - t_sne_exaggerates_clusters_provably__2_outliers.json - t_sne_exaggerates_clusters_provably__abstract.json - t_sne_exaggerates_clusters_provably__appendix.json - t_sne_exaggerates_clusters_provably__discussion.json - t_sne_exaggerates_clusters_provably__introduction.json - t_sne_exaggerates_clusters_provably__main.json - t_sne_exaggerates_clusters_provably__math_commands.json - t_sne_exaggerates_clusters_provably__preliminaries.json - t_sne_exaggerates_clusters_provably__related_work.json Modified: - constants.py - scientific.py - scientific_papers.ipynb

--- uml_project/data/constants.py
+++ uml_project/data/constants.py
@@ -7,6 +7,7 @@ ROOT_DIR = Path(os.environ.get("DATA_DIR") or Path(__file__).parent.parent.paren
 MUSIC_DIR = ROOT_DIR / "music/"
 SCRATCH_DIR = ROOT_DIR / "scratch/"  # for temporary files
 SCIENTIFIC_DIR = ROOT_DIR / "scientific/"
+SCIENTIFIC_REGISTRY = SCIENTIFIC_DIR / "REGISTRY.json"  # JSON file with list of paper URLs
 
 # KAGGLE
 TAYLOR_SWIFT_KAGGLE = "ishikajohari/taylor-swift-all-lyrics-30-albums"
@@ -17,8 +18,8 @@ GENIUS_ACCESS_TOKEN = os.environ.get("GENIUS_ACCESS_TOKEN")  # Store in .env fil
 
 
 # Dtypes
-class TexSource(t.TypedDict):
-    id: str | None  # e.g. "2503.19280v1"
+class TexSource(t.TypedDict, total=True):
+    id: str  # e.g. "2503.19280v1"
     files: list[dict[str, str]]  # {"name": str, "text": str}
     main: str | None  # main .tex file name
     title: str | None  # extracted title, if any

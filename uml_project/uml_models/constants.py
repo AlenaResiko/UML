36116f9295312adbf5f2712bfe514c6f47c9aded
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-11-04 00:40:34 -0500

    word2vec Added: - load_ex.ipynb - word2vec_google-news_300_v000.ipynb Modified: - constants.py - __init__.py - constants.py Deleted: - .gitkeep

--- uml_project/uml_models/constants.py
+++ uml_project/uml_models/constants.py
@@ -2,5 +2,5 @@ import os
 from pathlib import Path
 
 # PATHS
-ROOT_DIR = Path(os.environ.get("DATA_DIR") or Path(__file__).parent.parent.parent).resolve() / "data"
+ROOT_DIR = Path(os.environ.get("ROOT_DIR") or Path(__file__).parent.parent.parent).resolve()
 MODEL_DIR = ROOT_DIR / "models/"

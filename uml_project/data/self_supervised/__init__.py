ca281820d349cc493cfd731e3f0d5a8391f112eb
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-11-03 19:08:02 -0500

    Similarity-Score Added: - Word2Vec.ipynb Modified: - test_sentence.ipynb - __init__.py - synthetic_labels.py

--- uml_project/data/self_supervised/__init__.py
+++ uml_project/data/self_supervised/__init__.py
@@ -1,1 +1,1 @@
-from uml_project.data.self_supervised.synthetic_labels import generate_synthetic_position_labels
+from uml_project.data.self_supervised.synthetic_labels import synthetic_similarity_labels
